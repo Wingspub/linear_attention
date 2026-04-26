0faaf809b59c954b1f51fa241d29e7eb61a4c31d
Wingspub <wingszhangyijie@gmail.com>
2026-04-26 13:06:44 +0800

    init: build copy task

--- dataset/synthetic_dataset.py
+++ dataset/synthetic_dataset.py
@@ -3,7 +3,7 @@ from torch.utils.data import IterableDataset
 
 class SyntheticDataset(IterableDataset):
     '''人工合成数据'''
-    def __init__(self, TOKEN_num :int, seq_len :int):
+    def __init__(self, TOKEN_num: int, seq_len: int):
         self.TOEKN_num = TOKEN_num
         self.seq_len = seq_len
 
@@ -14,4 +14,17 @@ class SyntheticDataset(IterableDataset):
             yield seq
 
 
+class Synthetic4RepetionDataset(IterableDataset):
+    '''人工合成的重复数据'''
+    def __init__(self, TOKEN_num: int, src_seq_len: int):
+        super().__init__()
+        self.Token_num = TOKEN_num + 1                  # 一个额外的token给开始，默认为0
+        self.src_seq_len = src_seq_len
+        self.seq_len = 1 + src_seq_len + src_seq_len    # 形式为[start, src, start, src]
+
+    def __iter__(self):
+        while True:
+            src_seq = torch.randint(1, self.Token_num, size=(self.src_seq_len,))
+            seq = torch.concat([torch.tensor([0]), src_seq, src_seq])
+            yield seq
 
---
+++ task/synthetic_copy_task.py
@@ -0,0 +1,89 @@
+import torch
+from typing import cast
+from torch import optim
+from torch.nn import CrossEntropyLoss, Module
+from torch.utils.data import DataLoader
+from model.base_model import Base, MLP
+from dataset.synthetic_dataset import Synthetic4RepetionDataset
+from time import time
+
+print("this is a synthetic copy task")
+
+# config
+TOKEN_num = 10
+seq_len = 512
+iter_num = 100000
+print_num = 100
+lr = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+## model
+dims = 128
+# model = Base(TOKEN_num=TOKEN_num+1, dims=dims).to(device)
+model = MLP(TOKEN_num=TOKEN_num+1, dims=dims).to(device)
+
+# init
+dataset = Synthetic4RepetionDataset(TOKEN_num=TOKEN_num, src_seq_len=seq_len)
+dataloader = DataLoader(dataset=dataset, batch_size=32, num_workers=2)
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+loss_func = CrossEntropyLoss()
+
+def train(model: Module, seq_data: torch.Tensor, device: torch.device) -> float:
+    '''模型训练'''
+    model.train()
+    # source data and target data
+    X = seq_data[:-1].detach().clone().to(device)
+    Y = seq_data[1:].detach().clone().to(device)
+
+    y_pred = cast(torch.Tensor, model(X))
+
+    loss = cast(torch.Tensor, loss_func(y_pred.reshape(-1,TOKEN_num+1), Y.reshape(-1)))
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    return loss.cpu().item()
+
+
+def eval(modle: Module, seq_data: torch.Tensor, device: torch.device):
+    ...
+
+
+@torch.inference_mode()
+def generate(model: Module, seq_len: int=128) -> torch.Tensor:
+    '''生成字符'''
+    model.eval()
+    start_token = torch.tensor([0])
+    response = torch.zeros(seq_len+1, dtype=torch.int32)
+    response[0] = start_token
+
+    start = time()
+    for i in range(1, seq_len):
+        input_token = response[:i].detach().clone().cuda()
+        output_pred = model(input_token)
+        response[i+1] = torch.argmax(output_pred[-1].cpu())
+
+        # print(response)
+    end = time()
+    rate = seq_len / (end-start)
+    print(f"speed:{rate:.2f} tokens/s")
+    return response
+
+
+temp_step = 0
+for seq in dataloader:
+    loss = train(model=model, seq_data=seq, device=device)
+
+    if temp_step % print_num == 0: print(f"step:{temp_step+1}, loss:{loss:.6f}")
+
+    temp_step += 1
+    if temp_step >= iter_num:
+        break
+
+# 字符生成测试
+for _ in range(5):
+    res = generate(model, seq_len=128)
+    print("res:", res)
+
+print("over")
