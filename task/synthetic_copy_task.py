2f0db844e47c8c6ac34ba6262205896f3c9522b0
Wingspub <wingszhangyijie@gmail.com>
2026-04-27 16:32:44 +0800

    fix: update synthetic copy task for improved model training and evaluation

--- task/synthetic_copy_task.py
+++ task/synthetic_copy_task.py
@@ -3,30 +3,31 @@ from typing import cast
 from torch import optim
 from torch.nn import CrossEntropyLoss, Module
 from torch.utils.data import DataLoader
-from model.base_model import Base, MLP
+from model.base_model import MLP, SimpleSequentialModel
 from dataset.synthetic_dataset import Synthetic4RepetionDataset
 from time import time
 
 print("this is a synthetic copy task")
 
 # config
-TOKEN_num = 1+10 # 需要有一个token作为<BOS>
-seq_len = 511
+TOKEN_num = 1+30 # 需要有一个token作为<BOS>
+seq_len = 127
 iter_num = 100000
-print_num = 100
+loss_print_num = 100
+eval_num = 1000
 lr = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 ## model
 dims = 128
-# model = Base(TOKEN_num=TOKEN_num, dims=dims).to(device)
 model = MLP(TOKEN_num=TOKEN_num, dims=dims).to(device)
+# model = SimpleSequentialModel(TOKEN_num=TOKEN_num, dims=dims, device=device).to(device)
 
 # init
 dataset = Synthetic4RepetionDataset(TOKEN_num=TOKEN_num, src_seq_len=seq_len)
 dataloader = DataLoader(dataset=dataset, batch_size=32, num_workers=2)
 
-optimizer = optim.SGD(model.parameters(), lr=lr)
+optimizer = optim.Adam(model.parameters(), lr=lr)
 loss_func = CrossEntropyLoss()
 
 
@@ -48,20 +49,21 @@ def train(model: Module, seq_data: torch.Tensor, seq_len: int, device: torch.dev
 
 
 @torch.inference_mode()
-def generate(model: Module, src_seq: torch.Tensor, seq_len: int=128) -> torch.Tensor:
+def generate(model: Module, src_seq: torch.Tensor, seq_len: int, device: torch.device) -> torch.Tensor:
     '''生成字符'''
     model.eval()
-    src_len = len(src_seq)
+    batch, src_len = src_seq.shape
     assert src_len <= seq_len
 
-    response = torch.zeros(seq_len+1, dtype=torch.int32)
-    response[:src_len] = src_seq
+    response = torch.zeros((batch, seq_len+1), dtype=torch.int32)
+    response[:, :src_len] = src_seq
+
 
     start = time()
     for i in range(src_len, seq_len):
-        input_token = response[:i].detach().clone().cuda()
+        input_token = response[:, :i].detach().clone().to(device)
         output_pred = model(input_token)
-        response[i+1] = torch.argmax(output_pred[-1].cpu())
+        response[:, i+1] = torch.argmax(output_pred[:,-1].cpu(), dim=-1)
 
     end = time()
     rate = seq_len / (end-start)
@@ -72,26 +74,26 @@ def generate(model: Module, src_seq: torch.Tensor, seq_len: int=128) -> torch.Te
 @torch.inference_mode()
 def eval(model: Module, seq_data: torch.Tensor, seq_len: int, device: torch.device) -> float:
     model.eval()
-
-    input_seq = seq_data[0, :1+seq_len].detach().clone().cuda()
-    res = generate(model=model, src_seq=input_seq, seq_len=(1+seq_len*2))
+    batch = seq_data.shape[0]
+    input_seq = seq_data[:, :1+seq_len].detach().clone().to(device)
+    res = generate(model=model, src_seq=input_seq, seq_len=(1+seq_len*2), device=device)
 
     # acc
-    target_seq = seq_data[0, 1+seq_len:].detach().clone()
-    hat_seq = res[1+seq_len:].detach().clone()
-    acc = torch.sum(target_seq == hat_seq).item()/seq_len
+    target_seq = seq_data[:, 1+seq_len:].detach().clone()
+    hat_seq = res[:, 1+seq_len:].detach().clone()
+    acc = torch.sum(target_seq == hat_seq).item()/seq_len/batch
 
     return acc
 
 temp_step = 0
 for seq in dataloader:
-    loss = train(model=model, seq_data=seq, seq_len=seq_len, device=device)
-
-    if temp_step % print_num == 0: print(f"step:{temp_step+1}, loss:{loss:.6f}")
-
-    if temp_step % 1000 == 0:
+    if temp_step % eval_num == 0:
         acc = eval(model=model, seq_data=seq, seq_len=seq_len, device=device)
         print(f"acc:{acc:.6f}")
+
+    loss = train(model=model, seq_data=seq, seq_len=seq_len, device=device)
+
+    if temp_step % loss_print_num == 0: print(f"step:{temp_step+1}, loss:{loss:.6f}")
 
     temp_step += 1
     if temp_step >= iter_num:
