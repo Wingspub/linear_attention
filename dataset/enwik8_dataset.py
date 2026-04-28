87afa80a766862254aefb1a83226b4491061f359
Wingspub <wingszhangyijie@gmail.com>
2026-04-28 20:51:03 +0800

    feat: add Enwik8Dataset class for iterable dataset support

---
+++ dataset/enwik8_dataset.py
@@ -0,0 +1,14 @@
+from torch.utils.data import IterableDataset
+
+class Enwik8Dataset(IterableDataset):
+    def __init__(self):
+        super().__init__()
+
+        # 
+
+
+
+    def __iter__(self):
+        ...
+
+        return 
