3bbe1c2144df3454b9cb2b64f11bc49495d31e5c
Wingspub <wingszhangyijie@gmail.com>
2026-05-19 00:56:01 +0800

    feat: Add modern converter models and their related classes

---
+++ model/modern_transformer.py
@@ -0,0 +1,41 @@
+# (Llama) The Llama 3 Herd of Models
+# http://arxiv.org/abs/2407.21783
+
+# (RoPE) RoFormer: Enhanced Transformer with Rotary Position Embedding
+# http://arxiv.org/abs/2104.09864
+
+# (RMSNorm) Root Mean Square Layer Normalization
+# https://arxiv.org/abs/1910.07467
+
+# (SiLU) Swish: a Self-Gated Activation Function
+# https://arxiv.org/abs/1710.05941
+
+
+from typing import Any
+
+from torch import nn
+import torch
+
+class RMSNorm(nn.Module):
+    ...
+
+
+class FFN(nn.Module):
+    ...
+
+
+class Attention(nn.Module):
+    ...
+
+
+class AttentionBlock(nn.Module):
+    ...
+
+
+class ModernTransformer(nn.Module):
+    ...
+
+
+
+
+
